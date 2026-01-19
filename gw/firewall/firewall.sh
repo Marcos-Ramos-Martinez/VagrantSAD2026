bb37fad623e882aeb153302730db053ae2a4a084
Marcos <marcos03rm@gmail.com>
2026-01-19 10:53:51 +0100

    mas cosas en el firewall

--- gw/firewall/firewall.sh
+++ gw/firewall/firewall.sh
@@ -21,6 +21,23 @@ iptables -P OUTPUT DROP
 ###################################
 # Reglas de proteccion local
 ###################################
+# L1. Permitir trafico de loopback
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+
+# L2 Permitir ping a maquina externa e interna
+iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L3 Permitir que me hagan desde LAN y DMZ
+iptables -A INPUT -i eth2 -s 172.1.7.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -i eth3 -s 172.2.7.0/24 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A OUTPUT -o eth2 -s 172.1.7.1 -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT -o eth3 -s 172.2.7.1 -p icmp --icmp-type echo-reply -j ACCEPT
+
+# L4 Permitir consultas DNS
+iptables -A OUTPUT -o eth0 -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -i eth0 -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 ###################################
 # Reglas de proteccion de red
